67065733280faa99f954dedb9d82fc548942770c
Shamak Patel <patel8sd@mail.uc.edu>
2024-10-31 12:21:07 -0400

    first commit

---
+++ Controllers/NameController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class NameController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetName()
+    {
+        return Ok(new { Name = "Shamak Patel", Age = 24 });
+    }
+}
